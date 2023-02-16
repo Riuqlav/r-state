357fb1a5373827130e1b3fd760c8267277df6d19
Riuqlav <valquirsolza.vs@gmail.com>
2023-02-16 09:48:51 +0100

    Create pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
