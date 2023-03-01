3093dd99fdc3deb261865cbc93144745a803431a
Riuqlav <valquirsolza.vs@gmail.com>
2023-03-01 11:06:05 +0100

    setup auth and db

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
 const SignUP = () => {
   const [showPassword, setshowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
   const [formData, setformData] = useState({
     name: "",
     email: "",
@@ -17,6 +27,32 @@ const SignUP = () => {
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      const user = userCredential.user;
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timeStamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <section>
@@ -30,7 +66,7 @@ const SignUP = () => {
           />
         </div>
         <div className="w-full md:w-[65%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="text"
